bac510dfa387ef709419a53af8c702f9565edb68
febassetto1 <felipe.basseto.pb@compasso.com.br>
2022-08-25 10:38:34 -0300

    Fixing cors error

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express'
 import config from './config'
 import cors from 'cors'
 import router from './routes/index.routes'
-import bodyParser from 'body-parser'
-import User from './models/User'
 
 const app = express()
 
@@ -11,7 +9,7 @@ const port = config.PORT || 8080;
 
 app.use(express.json())
 
-app.use(cors({ credentials: true, origin: 'http://localhost:8080' }))
+app.use(cors())
 
 app.use(express.urlencoded({ extended: true }))
 
